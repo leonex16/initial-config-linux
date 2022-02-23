d515f9ed39831964dd6f8b5ba5ae4f16d17e1fa8
Leonel Antonio Espinoza Sotelo <55729995+leonex16@users.noreply.github.com>
2022-02-22 21:37:14 -0300

    Add qtile config

---
+++ Widgets.py
@@ -0,0 +1,29 @@
+from libqtile import widget
+
+from constants import WIDGETS_CONFIG
+
+def Widgets():
+  def workspaces():
+    return [
+        widget.GroupBox(**WIDGETS_CONFIG['GROUP_BOX'])
+    ]
+
+  def powerline_base():
+    return [
+      # widget.Wlan(),
+      # widget.KeyboardLayout(**WIDGETS_CONFIG['KEYBOARD']),
+      widget.Spacer(),
+      widget.Sep(**WIDGETS_CONFIG['SEP']),
+      widget.WindowName(),
+      widget.Sep(**WIDGETS_CONFIG['SEP']),
+      widget.PulseVolume(),
+      widget.Sep(**WIDGETS_CONFIG['SEP']),
+      widget.Battery(**WIDGETS_CONFIG['BATTERY']),
+      widget.Sep(**WIDGETS_CONFIG['SEP']),
+      widget.Clock( **WIDGETS_CONFIG['CLOCK']),
+    ]
+  
+  return [
+    *workspaces(),
+    *powerline_base()
+  ]
---
+++ constants.py
@@ -0,0 +1,87 @@
+
+from libqtile.utils import guess_terminal
+
+DEFAULT_APPLICATIONS = {
+  'TERMINAL': guess_terminal(),
+  'BROWSER': 'microsoft-edge-stable',
+  'FILE_MANAGER': 'thunar',
+  'VS_CODE': 'code'
+}
+
+MOD = "mod4"
+
+WALLPAPER = "~/.config/qtile/wallpaper.jpg"
+
+AUTOSTART = [
+  "xrandr --output eDP1 --mode 1920x1080 --output HDMI1 --mode 1920x1080 --right-of eDP1 &",
+  "picom &",
+  "setxkbmap -layout latam -variant dvorak &",
+]
+
+COLORS = {
+    'PRIMARY': '#00BCFF',
+    'WARNING': '#FFC00D',
+    'ERROR': '#FF430D',
+    'WHITE': '#FAFAFA',
+    'BLACK': '#222222',
+  }
+
+FONTS = {
+    'NORMAL': {
+      'FAMILY': 'Cascadia Code',
+      'SIZE': 16
+    }
+  }
+
+LAYOUT_CONFIG = {
+    'border_normal': COLORS["WHITE"],
+    'border_focus': COLORS["PRIMARY"],
+    'border_width': 4,
+    'margin': 8,
+  }
+
+WIDGET_DEFAULT = {
+  'font': FONTS["NORMAL"]["FAMILY"],
+  'fontsize': FONTS["NORMAL"]["SIZE"],
+  'padding': 4,
+}
+
+WIDGETS_CONFIG = {
+  'GROUP_BOX': {
+      'font': FONTS['NORMAL']['FAMILY'],
+      'fontsize': FONTS['NORMAL']['SIZE']
+  },
+
+  'CLOCK': {
+      'font': FONTS['NORMAL']['FAMILY'],
+      'fontsize': FONTS['NORMAL']['SIZE'],
+      'format': '%d / %m / %Y - %H:%M '
+      #'timezone' : 'Chile/Cotinental'
+  },
+
+  'BATTERY': {
+      'font': FONTS['NORMAL']['FAMILY'],
+      'fontsize': FONTS['NORMAL']['SIZE'],
+      'charge_char' : 'C',
+      'discharge_char' : 'D',
+      'full_char' : 'F',
+      'format' : '{char} {percent:2.0%}'
+  },
+
+  'KEYBOARD': {
+      'font': FONTS['NORMAL']['FAMILY'],
+      'fontsize': FONTS['NORMAL']['SIZE'],
+      'configured_keyboards' : ['dvorak-la'],
+      'display_map' : {'es' : 'cl'},
+  },
+
+  'SEP': {
+      'foreground' : COLORS["BLACK"]
+  },
+}
+
+
+SCREEN_CONFIG = {
+  'wallpaper': WALLPAPER,
+  'wallpaper_mode': 'fill',
+}
